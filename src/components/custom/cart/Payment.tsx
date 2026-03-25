c10e34a5e9ada895743c0ef2d5ca54fd0fe9609f
suttirakcha <suttirak.char@gmail.com>
2026-03-25 16:58:54 +0700

    feat (cart): update the style of cart item and payment

--- src/components/custom/cart/Payment.tsx
+++ src/components/custom/cart/Payment.tsx
@@ -4,11 +4,23 @@ import { Button } from '@/components/ui/button';
 
 export default function Payment() {
   return (
-    <div className='border rounded-xl p-6'>
+    <div className='border rounded-xl p-6 space-y-6'>
+      <h2 className='font-bold text-2xl'>สรุปรายการ</h2>
       <form className='flex flex-col gap-4'>
-        <div className='flex gap-2'>
-          <Input type='text' />
-          <Button>ใช้</Button>
+        <div className='space-y-1'>
+          <h2 className='font-semibold'>รหัสคูปอง</h2>
+          <div className='flex gap-2'>
+            <Input type='text' placeholder='ใส่รหัสคูปอง' />
+            <Button variant='outline'>ใช้</Button>
+          </div>
+        </div>
+        <div className='flex items-center justify-between'>
+          <h1>ราคารวม:</h1>
+          <p className='font-medium'>฿1,290</p>
+        </div>
+        <div className='flex items-center justify-between'>
+          <h1 className='font-bold text-xl'>ยอดรวมทั้งหมด:</h1>
+          <p className='text-primary font-bold text-xl'>฿1,290</p>
         </div>
         <LnButton>ชำระเงิน</LnButton>
         <p className='text-muted-foreground text-xs text-center'>
