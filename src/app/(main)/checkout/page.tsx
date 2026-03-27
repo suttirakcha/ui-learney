1456c6031ec982cf02a97b1b8ecca93771931666
suttirakcha <suttirak.char@gmail.com>
2026-03-27 17:26:23 +0700

    feat (cart): set up the form submission of the checkout

--- src/app/(main)/checkout/page.tsx
+++ src/app/(main)/checkout/page.tsx
@@ -1,22 +1,20 @@
-import CheckoutSummary from '@/components/cart/CheckoutSummary';
+'use client';
+
 import CheckoutForm from '@/components/features/checkout-form';
 import { Fragment } from 'react';
 
 export default function CheckoutPage() {
   return (
     <Fragment>
-      <div className='bg-primary px-8 py-12'>
-        <h2 className='text-4xl font-medium text-white'>ชำระเงิน</h2>
+      <div className='bg-primary px-8 py-16'>
+        <div className='max-w-7xl w-full mx-auto'>
+          <h2 className='text-5xl font-medium text-white'>ชำระเงิน</h2>
+        </div>
       </div>
       <div className='p-8'>
-        <main className='grid grid-cols-3 gap-6'>
-          <section className='col-span-2'>
-            <CheckoutForm />
-          </section>
-          <section>
-            <CheckoutSummary />
-          </section>
-        </main>
+        <div className='max-w-7xl w-full mx-auto'>
+          <CheckoutForm />
+        </div>
       </div>
     </Fragment>
   );
