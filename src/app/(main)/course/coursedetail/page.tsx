ae6ef28342c589856f0ec299a09f9d61c3ec3c08
wargreymon44 <criminalice2@gmail.com>
2026-03-27 12:33:34 +0700

    feat (coursedetail): update title

--- src/app/(main)/course/coursedetail/page.tsx
+++ src/app/(main)/course/coursedetail/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'CourseDetail',
+};
+
 import CourseDetailsPage from '@/components/course/coursedetails/DetailPage';
 
 export default function DetailCourse() {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Navbar from "@/components/custom/Navbar";
-import Hero from "@/components/custom/Hero";
-import Stats from "@/components/custom/Stats";
-import CourseList from "@/components/custom/CourseList";
-import CTA from "@/components/custom/CTA";
-import Footer from "@/components/custom/Footer";
+import Hero from '@/components/custom/Hero';
+import Stats from '@/components/custom/Stats';
+import CourseList from '@/components/custom/CourseList';
+import CTA from '@/components/custom/CTA';
+import Footer from '@/components/custom/Footer';
 
 export default function HomePage() {
   return (
