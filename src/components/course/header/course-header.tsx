7aeb0959abf64a401941b23718ca277ea7d0d332
suttirakcha <suttirak.char@gmail.com>
2026-03-30 15:33:58 +0700

    feat (home): remove the course header from the homepage

--- src/components/course/header/course-header.tsx
+++ src/components/course/header/course-header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 interface CourseHeaderProps {
   categoryName: string;
@@ -6,11 +6,11 @@ interface CourseHeaderProps {
 
 const CourseHeader: React.FC<CourseHeaderProps> = ({ categoryName }) => {
   return (
-    <div className="bg-gradient-to-r from-cyan-400 to-cyan-300 px-8 py-16">
-      <div className="max-w-7xl mx-auto">
-        <h1 className="text-5xl font-black text-white mb-4">{categoryName}</h1>
-        <div className="flex items-center gap-3">
-          <p className="text-white text-lg">
+    <div className='bg-linear-to-r from-cyan-400 to-cyan-300 px-8 py-16'>
+      <div className='max-w-7xl mx-auto'>
+        <h1 className='text-5xl font-black text-white mb-4'>{categoryName}</h1>
+        <div className='flex items-center gap-3'>
+          <p className='text-white text-lg'>
             ค้นพบคอร์สเรียนที่หลากหลาย และเริ่มต้นเรียนรู้ได้แล้ววันนี้
           </p>
         </div>
