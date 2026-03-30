dde43e88d46fbace86f322868436be385f07844a
suttirakcha <suttirak.char@gmail.com>
2026-03-30 16:25:03 +0700

    fix (home): remove unused comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,70 +1,24 @@
-// import type { Metadata } from "next";
-// import { Kanit, Geist } from "next/font/google";
-// import { cn } from "@/lib/utils";
-// import "./globals.css";
-// import { Toaster } from "react-hot-toast";
-// import Navbar from "@/components/custom/Navbar";
+import type { Metadata } from 'next';
+import { Kanit, Geist } from 'next/font/google';
+import { cn } from '@/lib/utils';
+import './globals.css';
+import { Toaster } from 'react-hot-toast';
+import Navbar from '@/components/custom/Navbar';
+import { AuthProvider } from '@/app/lib/AuthContext'; // ✅ เพิ่ม
 
-// const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
-
-// const kanit = Kanit({
-//   subsets: ["latin", "thai"],
-//   weight: ["400", "500", "600"],
-// });
-
-// export const metadata: Metadata = {
-//   title: {
-//     default: "Learney",
-//     template: "%s - Learney",
-//   },
-//   description: "...",
-// };
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <html
-//       lang="en"
-//       className={cn(
-//         "h-full",
-//         "antialiased",
-//         kanit.className,
-//         "font-sans",
-//         geist.variable,
-//       )}
-//     >
-//       <body className="min-h-full flex flex-col">
-//         <Navbar />
-//         {children}
-//         <Toaster position="top-center" />
-//       </body>
-//     </html>
-//   );
-// }
-import type { Metadata } from "next";
-import { Kanit, Geist } from "next/font/google";
-import { cn } from "@/lib/utils";
-import "./globals.css";
-import { Toaster } from "react-hot-toast";
-import Navbar from "@/components/custom/Navbar";
-import { AuthProvider } from "@/app/lib/AuthContext"; // ✅ เพิ่ม
-
-const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
+const geist = Geist({ subsets: ['latin'], variable: '--font-sans' });
 
 const kanit = Kanit({
-  subsets: ["latin", "thai"],
-  weight: ["400", "500", "600"],
+  subsets: ['latin', 'thai'],
+  weight: ['400', '500', '600'],
 });
 
 export const metadata: Metadata = {
   title: {
-    default: "Learney",
-    template: "%s - Learney",
+    default: 'Learney',
+    template: '%s - Learney',
   },
-  description: "...",
+  description: '...',
 };
 
 export default function RootLayout({
@@ -74,22 +28,22 @@ export default function RootLayout({
 }) {
   return (
     <html
-      lang="en"
+      lang='en'
       className={cn(
-        "h-full",
-        "antialiased",
+        'h-full',
+        'antialiased',
         kanit.className,
-        "font-sans",
-        geist.variable,
+        'font-sans',
+        geist.variable
       )}
     >
-      <body className="min-h-full flex flex-col">
+      <body className='min-h-full flex flex-col'>
         <AuthProvider>
-          {" "}
+          {' '}
           {/* ✅ ครอบตรงนี้ */}
           <Navbar />
           {children}
-          <Toaster position="top-center" />
+          <Toaster position='top-center' />
         </AuthProvider>
       </body>
     </html>
