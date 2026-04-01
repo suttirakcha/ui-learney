e4ba74788796f70f8a5a043f1436169ca09b1664
winepnp-code <wine.pnprinz99@gmail.com>
2026-04-01 11:40:24 +0700

    chore: Delete admin file

--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -1,6 +1,6 @@
-import { Course } from '@/types/course';
-import { Trash2 } from 'lucide-react';
-import LnButton from '../custom/LnButton';
+import { Course } from "@/types/course/course";
+import { Trash2 } from "lucide-react";
+import LnButton from "../custom/LnButton";
 
 interface CartItemProps {
   course: Course;
@@ -10,29 +10,29 @@ export default function CartItem({ course }: CartItemProps) {
   const { courseName, price, instructor } = course;
 
   const priceAmount =
-    typeof price === 'number' ? price?.toLocaleString() : price;
+    typeof price === "number" ? price?.toLocaleString() : price;
 
   return (
-    <div className='border rounded-xl p-6'>
-      <div className='flex justify-between'>
-        <div className='flex gap-4'>
+    <div className="border rounded-xl p-6">
+      <div className="flex justify-between">
+        <div className="flex gap-4">
           {/* Image */}
-          <div className='bg-gray-300 rounded-md w-30 h-20'></div>
+          <div className="bg-gray-300 rounded-md w-30 h-20"></div>
 
           {/* Course detail */}
-          <div className='space-y-2'>
-            <div className='space-y-1'>
-              <h1 className='text-2xl font-bold'>{courseName}</h1>
-              <p className='text-muted-foreground'>โดย {instructor}</p>
+          <div className="space-y-2">
+            <div className="space-y-1">
+              <h1 className="text-2xl font-bold">{courseName}</h1>
+              <p className="text-muted-foreground">โดย {instructor}</p>
             </div>
-            <h2 className='text-primary font-bold text-xl'>฿{priceAmount}</h2>
+            <h2 className="text-primary font-bold text-xl">฿{priceAmount}</h2>
           </div>
         </div>
 
         {/* Delete item from cart */}
         <LnButton
-          variant='ghost'
-          className='flex items-center gap-2 text-destructive'
+          variant="ghost"
+          className="flex items-center gap-2 text-destructive"
         >
           <Trash2 />
           ลบ
---
+++ src/types/Proflie/admin/index.tsx
@@ -0,0 +1,16 @@
+// export interface AdminProfile {
+//   name: string;
+//   email: string;
+//   role: string;
+// }
+// export interface DashboardMetric {
+//   label: string;
+//   value: string | number;
+//   iconType: "users" | "revenue" | "course" | "alert";
+// }
+// export interface AdminAlert {
+//   id: string;
+//   message: string;
+//   actionLabel: string;
+//   type: "warning" | "info" | "error" | "success";
+// }
