5fd245f5b1d378d3c6ec74d637bd5594798fb01e
suttirakcha <suttirak.char@gmail.com>
2026-04-01 13:48:27 +0700

    fix (profile): define the role to be used with the whole profile page

--- src/app/(main)/profile/[path]/page.tsx
+++ src/app/(main)/profile/[path]/page.tsx
@@ -1,5 +1,9 @@
-import { getMenusWithRole } from "@/lib/profile-menus";
-import { Fragment } from "react";
+import ProfileHeader from '@/components/profile/ProfileHeader';
+import ProfileSidebar from '@/components/profile/ProfileSidebar';
+import { getMenusWithRole } from '@/lib/profile-menus';
+import { Role } from '@/types/user';
+
+const USER_ROLE: Role = 'ADMIN';
 
 interface InstructorProfilePageProps {
   params: { path: string };
@@ -7,7 +11,7 @@ interface InstructorProfilePageProps {
 
 export async function generateMetadata({ params }: InstructorProfilePageProps) {
   const { path } = await params;
-  const menus = getMenusWithRole("INSTRUCTOR");
+  const menus = getMenusWithRole(USER_ROLE);
 
   const profileLabel = menus.find((menu) => menu.href === `/${path}`)?.label;
 
@@ -20,11 +24,21 @@ export default async function InstructorProfilePage({
   params,
 }: InstructorProfilePageProps) {
   const { path } = await params;
-  const menus = getMenusWithRole("INSTRUCTOR");
+  const menus = getMenusWithRole(USER_ROLE);
 
   const profileComponent = menus.find(
-    (menu) => menu.href === `/${path}`,
+    (menu) => menu.href === `/${path}`
   )?.component;
 
-  return <Fragment>{profileComponent}</Fragment>;
+  return (
+    <div>
+      <ProfileHeader />
+      <div className='max-w-7xl w-full mx-auto grid grid-cols-1 lg:grid-cols-4 gap-10 p-8'>
+        <div>
+          <ProfileSidebar role={USER_ROLE} />
+        </div>
+        <div className='lg:col-span-3'>{profileComponent}</div>
+      </div>
+    </div>
+  );
 }
